724d889610a1833beb43c8396b4c935c1ebe655d
TranChinh2901 <tranchinht32901@gmail.com>
2024-12-16 23:22:40 +0700

    update code

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,11 +2,19 @@ const express = require('express');
 
 const formidable = require('express-formidable');
 const { requireSignIn, isAdmin } = require('../middlewares/authMiddleware');
-const { createProductController } = require('../controllers/productController');
+const { createProductController, getProductController, getSingleProductController, productPhotoController, deleteProductController, updateProductController } = require('../controllers/productController');
 
 const router = express.Router();
 
 router.post('/create-product', requireSignIn, isAdmin, formidable(), createProductController)
 
+router.put('/update-product/:pid', requireSignIn, isAdmin, formidable(), updateProductController)
 
+router.get('/get-product', getProductController)
+
+router.get('/get-product/:slug', getSingleProductController)
+
+router.get("/product-photo/:pid", productPhotoController);
+
+router.get('/product/:pid', deleteProductController)
 module.exports = router
